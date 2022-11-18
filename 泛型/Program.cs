1b735ed693d8e76e9fa83cc547330a8d7a0ff592
lml0126 <lml0126@163.com>
2022-11-18 20:22:24 +0800

    添加项目文件。

---
+++ 泛型/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泛型
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////
+            Mydele<int> mydele = new Mydele<int>(Add);
+           int res= mydele(100,300);
+            Console.WriteLine(res);
+            //////////////////////////////////////
+            Mydele<double> mydele1= new Mydele<double>(Mul);
+            double res1 = mydele1(200, 5);
+            Console.WriteLine(res1);
+            /////////////////////////////////////////
+            Console.ReadLine();
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static double Mul(double x, double y)
+        {
+            return x * y;
+        }
+    }
+    delegate T Mydele<T>(T a,T b);  //只声明一个委托，可以使用在不同的数据类型上
+}
